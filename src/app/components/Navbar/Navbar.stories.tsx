2541e059ce6a248bbff4dbd5f0eb681b75b8768b
Paul Sheehan <p.sheehan94@gmail.com>
2023-05-23 15:47:56 +0100

    Added dev envionment items

---
+++ src/app/components/Navbar/Navbar.stories.tsx
@@ -0,0 +1,12 @@
+// Navbar.stories.ts|tsx
+
+import type { Meta } from '@storybook/react';
+
+import Navbar from './Navbar';
+
+const meta: Meta<typeof Navbar> = {
+  title: 'Navbar',
+  component: Navbar,
+};
+
+export default meta;
---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+      <nav>
+        <span>Icon</span>
+        <ul>
+            <li><a href="#">Episodes</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">More</a></li>
+        </ul>
+      </nav>
+    );
+}
